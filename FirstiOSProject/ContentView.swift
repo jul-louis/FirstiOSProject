f8dbafd5f6e4e056acf9f8563d5aac0bab016a11
Louis Kano <louiskano.work@gmail.com>
2024-04-05 22:51:20 -0400

    Initial Commit

---
+++ FirstiOSProject/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FirstiOSProject
+//
+//  Created by Louis Kano on 2024-04-05.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FirstiOSProject/FirstiOSProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstiOSProjectApp.swift
+//  FirstiOSProject
+//
+//  Created by Louis Kano on 2024-04-05.
+//
+
+import SwiftUI
+
+@main
+struct FirstiOSProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
